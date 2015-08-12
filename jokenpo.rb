d2d7a318c2cb51d0bea0e649be73ed334777aa3d
Dojo People <dojo@thoughtworks.com>
2015-08-12 20:11:27 -0300

    rock draws with rock

--- jokenpo.rb
+++ jokenpo.rb
@@ -1,13 +1,22 @@
 class Jokenpo
 
+  ROCK = "rock"
+  PAPER = "paper"
+  SCISSORS = "scissors"
+  DRAW = "draw"
+
   def winner(first_hand, second_hand)
     winner = nil
-    if first_hand == "rock"
-      if second_hand == "paper"
-        winner = "paper"
+    
+    if first_hand == ROCK
+      if second_hand == PAPER
+        winner = PAPER
+      elsif second_hand == SCISSORS
+        winner = ROCK
       else
-        winner = "rock"
+        winner = DRAW
       end
+
     end
     winner
   end
--- spec/jokenpo_spec.rb
+++ spec/jokenpo_spec.rb
@@ -3,21 +3,27 @@ require_relative '../jokenpo'
 
 
 describe Jokenpo do
+  
+  let (:rock) { Jokenpo::ROCK }
+  let (:paper) { Jokenpo::PAPER }
+  let (:scissors) { Jokenpo::SCISSORS }
+  let (:draw) { Jokenpo::DRAW }
+
   subject { Jokenpo.new }
   
   context "rock" do
     it "beats scissors" do
-      scissors = "scissors"
-      rock = "rock"
       winner = subject.winner rock, scissors
       expect(winner).to eq rock
     end
 
     it "loses to paper" do
-      paper = "paper"
-      rock = "rock"
       winner = subject.winner rock, paper
       expect(winner).to eq paper
+    end
+
+    it "draws" do
+      expect(subject.winner(rock, rock)).to eq draw
     end
   end
 end
