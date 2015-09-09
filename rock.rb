31b8243663a75284d65267562134552c18a02aa9
BH Dojos <bh-dojos@thoughtworks.com>
2015-09-09 19:53:38 -0300

    Refatoracao do removendo o instanceof

--- rock.rb
+++ rock.rb
@@ -3,23 +3,23 @@ require_relative './paper'
 
 class Rock
 
-	def winner(other_hand)
+    def winner(other_hand)
 
-		if other_hand.instance_of? Rock
-			return :draw
-		end
+        if other_hand.instance_of? Rock
+            return :draw
+        end
 
-		if other_hand.instance_of? Scissors
-			return :rock
-		end
+        if other_hand.instance_of? Scissors
+            return :rock
+        end
 
-		if other_hand.is :paper
-			return :paper
-		end
+        if other_hand.is :paper
+            return :paper
+        end
 
-	end
+    end
 
     def is(type)
-    	:rock == type
-  	end
+        :rock == type
+    end
 end
