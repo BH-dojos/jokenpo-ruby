e79d8865f7ab474e7758bdf99303a0d0aaf1828c
BH Dojos <bh-dojos@thoughtworks.com>
2015-09-09 19:50:49 -0300

    correcao no teste paper

--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -4,6 +4,10 @@ require_relative '../paper'
 describe Paper do
 
   it "checks if it is paper" do
-    expect(subject.is(:paper))
+    expect(subject.is(:paper)).to be true
+  end
+
+  it "checks if it is not paper" do
+    expect(subject.is(:rock)).to be false
   end
 end
