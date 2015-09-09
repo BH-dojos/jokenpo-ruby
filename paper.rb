747fc35c8fc124c1af93badaf95343a7cb2e5e39
BH Dojos <bh-dojos@thoughtworks.com>
2015-09-09 20:18:12 -0300

    Inclusao dos testes para paper.

--- paper.rb
+++ paper.rb
@@ -1,5 +1,22 @@
 class Paper
-  def is(type)
-    :paper == type
-  end
+	def is(type)
+		:paper == type
+	end
+
+	def winner(other_hand)
+
+		if other_hand.is :paper
+			return :draw
+		end        
+
+		if other_hand.is :rock
+			return :paper
+		end
+
+		if other_hand.is :scissors
+			return :scissors
+		end        
+
+	end
+
 end
--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 require_relative '../paper'
 
 describe Paper do
+  let (:scissors){ Scissors.new }
+  let (:paper){ Paper.new }
+  let (:rock){ Rock.new }
+
 
   it "checks if it is paper" do
     expect(subject.is(:paper)).to be true
@@ -10,4 +14,21 @@ describe Paper do
   it "checks if it is not paper" do
     expect(subject.is(:rock)).to be false
   end
+   
+   it "beats rock" do
+    winner = subject.winner rock
+    expect(winner).to eq :paper
+  end
+  
+   it "loses scissors" do
+    winner = subject.winner scissors
+    expect(winner).to eq :scissors
+  end
+
+  it "draw with paper" do
+    winner = subject.winner paper
+    expect(winner).to eq :draw
+  end
+
+
 end
